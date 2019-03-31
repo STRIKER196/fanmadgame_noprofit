26caa96f4e4ee725f487dd64c55779ba67a698da
Michał Drab <michal.drab95@gmail.com>
2019-03-31 04:31:58 +0200

    shop

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,6 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-    public int skillPoints = 0;
-    public int u_dzida = 0;
-    public int u_lpg = 0;
-    public int u_osad = 0;
     public int levelPoints = 0;
     public GameObject score;
     private Text textPoints;
@@ -27,51 +23,12 @@ public class PlayerController : MonoBehaviour {
         }
 	}
 
-    public void UlepszDzide() {
-        if (skillPoints > 0)
-        {
-            if (u_dzida < 3)
-            {
-                u_dzida += 1;
-                skillPoints -= 1;
-            }
-        }
-    }
-
-    public void UlepszLpg()
-    {
-        if (skillPoints > 0)
-        {
-            if (u_lpg < 3)
-            {
-                u_lpg += 1;
-                skillPoints -= 1;
-            }
-        }
-    }
-
-    public void UlepszOsad()
-    {
-        if (skillPoints > 0)
-        {
-            if (u_osad < 3)
-            {
-                u_osad += 1;
-                skillPoints -= 1;
-            }
-        }
-    }
-
-    public void DodajPunktUmiejetnosci() {
-        skillPoints += 1;
-    }
-
     public void DodajPunktyPoziomu(int ileDodac) {
         levelPoints += ileDodac;
         textPoints.text = "Score: " + levelPoints.ToString() + "/100";
     }
 
     public void OtworzSklep() {
-        SceneManager.LoadScene("Sklep", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Sklep", LoadSceneMode.Single);
     }
 }
---
+++ Assets/Scripts/ShopController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour {
+
+    int dzida;
+    int lpg;
+    int osad;
+    public Sprite tak;
+    public Sprite nie;
+    SpriteRenderer otherObjectRenderer;
+    // Use this for initialization
+    void Start () {
+        dzida = PlayerPrefs.GetInt("dzida");
+        lpg = PlayerPrefs.GetInt("lpg");
+        osad = PlayerPrefs.GetInt("osad");
+        SetSkillsIcons();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    //najbardziej
+    //gowniana
+    //metoda
+    //ever
+    //prosze
+    //tu
+    //nie
+    //zagladac
+    void SetSkillsIcons() {
+        setDzida();
+        SetLpg();
+        SetOsad();
+    }
+
+    void setDzida() {
+        {
+            string nazwa = "dzida";
+            for (int i = 1; i < 4; i++)
+            {
+                otherObjectRenderer = GameObject.Find(nazwa + i.ToString()).GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = nie;
+            }
+            if (dzida >= 1)
+            {
+                otherObjectRenderer = GameObject.Find("dzida1").GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = tak;
+            }
+            if (dzida >= 2)
+            {
+                otherObjectRenderer = GameObject.Find("dzida2").GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = tak;
+            }
+            if (dzida == 3)
+            {
+                otherObjectRenderer = GameObject.Find("dzida3").GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = tak;
+            }
+        }
+    }
+    void SetLpg()
+    {
+        string nazwa = "lpg";
+        for (int i = 1; i < 4; i++)
+        {
+            otherObjectRenderer = GameObject.Find(nazwa + i.ToString()).GetComponent<SpriteRenderer>();
+            otherObjectRenderer.sprite = nie;
+        }
+        if (dzida >= 1)
+        {
+            otherObjectRenderer = GameObject.Find("lpg1").GetComponent<SpriteRenderer>();
+            otherObjectRenderer.sprite = tak;
+        }
+        if (dzida >= 2)
+        {
+            otherObjectRenderer = GameObject.Find("lpg2").GetComponent<SpriteRenderer>();
+            otherObjectRenderer.sprite = tak;
+        }
+        if (dzida == 3)
+        {
+            otherObjectRenderer = GameObject.Find("lpg3").GetComponent<SpriteRenderer>();
+            otherObjectRenderer.sprite = tak;
+        }
+    }
+    void SetOsad() {
+        {
+            string nazwa = "osad";
+            for (int i = 1; i < 4; i++)
+            {
+                otherObjectRenderer = GameObject.Find(nazwa + i.ToString()).GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = nie;
+            }
+            if (dzida >= 1)
+            {
+                otherObjectRenderer = GameObject.Find("osad1").GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = tak;
+            }
+            if (dzida >= 2)
+            {
+                otherObjectRenderer = GameObject.Find("osad2").GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = tak;
+            }
+            if (dzida == 3)
+            {
+                otherObjectRenderer = GameObject.Find("osad3").GetComponent<SpriteRenderer>();
+                otherObjectRenderer.sprite = tak;
+            }
+        }
+    }
+}
